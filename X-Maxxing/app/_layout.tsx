f469cf9e29694dec83d68c78fb837138d8935f1c
Nicky Lopez <nickylopez@outlook.de>
2025-04-17 18:47:08 +0200

    Experimented with Screens and Tabs

--- X-Maxxing/app/_layout.tsx
+++ X-Maxxing/app/_layout.tsx
@@ -8,6 +8,10 @@ import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
+import * as SQLite from 'expo-sqlite';
+
+
+
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
@@ -26,6 +30,18 @@ export default function RootLayout() {
   if (!loaded) {
     return null;
   }
+
+  /*
+  // DB STUFF
+  const dbname = 'mydb.db';
+  const db= SQLite.openDatabaseSync('@/database/' + dbname); // Open the database
+  const dbInitialQuery = 'CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, firstname TEXT, email TEXT, password Text)'; // Initial query to check if the table exists
+
+
+  useEffect (() => {
+    // Create a table if it doesn't exist
+    db.execSync(dbInitialQuery); // Execute the initial query
+  });*/
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
---
+++ X-Maxxing/database/Database.tsx
@@ -0,0 +1,7 @@
+import * as SQLite from 'expo-sqlite';
+import { useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
+
+export default function useDatabase() {
+  
+}
